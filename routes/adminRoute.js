aee85f0b84047a80870855eef20b0a632acde939
DevikaRajesh22 <devikaraj699@gmail.com>
2023-11-07 15:51:33 +0530

    adminAuth and userAuth called on routes

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,9 +1,9 @@
 const adminController = require('../controllers/adminController');
 const productController = require('../controllers/productController');
 const categoryController = require('../controllers/categoryController');
-const orderController=require('../controllers/orderController');
-const couponController=require('../controllers/couponController');
-const adminAuth=require('../middleware/adminAuth');
+const orderController = require('../controllers/orderController');
+const couponController = require('../controllers/couponController');
+const adminAuth = require('../middleware/adminAuth');
 const express = require('express');
 const adminRoute = express();
 const multer = require('../middleware/multer');
@@ -13,47 +13,47 @@ adminRoute.set('views', './views');
 adminRoute.set('view engine', 'ejs');
 
 //admin routes
-adminRoute.get('/',adminAuth.isLoggedOut, adminController.loginGet);
+adminRoute.get('/', adminAuth.isLoggedOut, adminController.loginGet);
 adminRoute.post('/login', adminController.loginPost);
-adminRoute.get('/landing',adminAuth.isLoggedIn, adminController.landing);
-adminRoute.get('/signout',adminController.signout);
+adminRoute.get('/landing', adminAuth.isLoggedIn, adminController.landing);
+adminRoute.get('/signout', adminAuth.isLoggedIn, adminController.signout);
 
 //user management
-adminRoute.get('/users',adminAuth.isLoggedIn, adminController.users);
-adminRoute.get('/block/:id', adminController.blockUser);
-adminRoute.get('/unblock/:id', adminController.unblockUser);
+adminRoute.get('/users', adminAuth.isLoggedIn, adminController.users);
+adminRoute.get('/block/:id', adminAuth.isLoggedIn, adminController.blockUser);
+adminRoute.get('/unblock/:id', adminAuth.isLoggedIn, adminController.unblockUser);
 
 //product management
-adminRoute.get('/products',adminAuth.isLoggedIn, productController.products);
-adminRoute.get('/addProducts',adminAuth.isLoggedIn, productController.addProducts);
-adminRoute.post('/addproducts',multer.upload.fields([{name : "image1", maxCount : 1},{name : "image2", maxCount : 1},{name : "image3", maxCount : 1},{name : "image4", maxCount : 1}]),productController.addProductsPost);
-adminRoute.get('/pedit', productController.editProduct);
-adminRoute.post('/pedit', multer.upload.fields([{name:"image1",maxCount:1},{name:"image2",maxCount:1},{name:"image3",maxCount:1},{name:"image4",maxCount:1}]), productController.editProductPost);
-adminRoute.get('/hide/:id',productController.hideProduct);
-adminRoute.get('/show/:id',productController.showProduct);
+adminRoute.get('/products', adminAuth.isLoggedIn, productController.products);
+adminRoute.get('/addProducts', adminAuth.isLoggedIn, productController.addProducts);
+adminRoute.post('/addproducts', adminAuth.isLoggedIn, multer.upload.fields([{ name: "image1", maxCount: 1 }, { name: "image2", maxCount: 1 }, { name: "image3", maxCount: 1 }, { name: "image4", maxCount: 1 }]), productController.addProductsPost);
+adminRoute.get('/pedit', adminAuth.isLoggedIn, productController.editProduct);
+adminRoute.post('/pedit', adminAuth.isLoggedIn, multer.upload.fields([{ name: "image1", maxCount: 1 }, { name: "image2", maxCount: 1 }, { name: "image3", maxCount: 1 }, { name: "image4", maxCount: 1 }]), productController.editProductPost);
+adminRoute.get('/hide/:id', adminAuth.isLoggedIn, productController.hideProduct);
+adminRoute.get('/show/:id', adminAuth.isLoggedIn, productController.showProduct);
 
 //category management
 adminRoute.get('/category', adminAuth.isLoggedIn, categoryController.category);
-adminRoute.get('/addCategory', categoryController.addCategory);
-adminRoute.post('/addCategoryPost', categoryController.addCategoryPost);
-adminRoute.get('/cblock', categoryController.blockCategory);
-adminRoute.get('/cunblock', categoryController.unblockCategory);
-adminRoute.get('/cedit', categoryController.editCategory);
-adminRoute.post('/cedit', categoryController.editCategoryPost);
+adminRoute.get('/addCategory', adminAuth.isLoggedIn, categoryController.addCategory);
+adminRoute.post('/addCategoryPost', adminAuth.isLoggedIn, categoryController.addCategoryPost);
+adminRoute.get('/cblock', adminAuth.isLoggedIn, categoryController.blockCategory);
+adminRoute.get('/cunblock', adminAuth.isLoggedIn, categoryController.unblockCategory);
+adminRoute.get('/cedit', adminAuth.isLoggedIn, categoryController.editCategory);
+adminRoute.post('/cedit', adminAuth.isLoggedIn, categoryController.editCategoryPost);
 
 //order management
 adminRoute.get('/ordersAdmin', adminAuth.isLoggedIn, orderController.orderGet);
-adminRoute.post('/updateStatus',orderController.updateStatus);
-adminRoute.get('/orderDetails', orderController.orderDetails);
+adminRoute.post('/updateStatus', adminAuth.isLoggedIn, orderController.updateStatus);
+adminRoute.get('/orderDetails', adminAuth.isLoggedIn, orderController.orderDetails);
 
 //coupon management
 adminRoute.get('/coupon', adminAuth.isLoggedIn, couponController.coupon);
-adminRoute.get('/addCoupon',adminAuth.isLoggedIn, couponController.addCoupon);
-adminRoute.post('/addCouponPost',couponController.addCouponPost);
-adminRoute.get('/editCoupon', couponController.editCoupon);
-adminRoute.post('/editCouponPost',couponController.editCouponPost);
-adminRoute.get('/hideCoupon',couponController.hideCoupon);
-adminRoute.get('/showCoupon',couponController.showCoupon);
+adminRoute.get('/addCoupon', adminAuth.isLoggedIn, couponController.addCoupon);
+adminRoute.post('/addCouponPost', adminAuth.isLoggedIn, couponController.addCouponPost);
+adminRoute.get('/editCoupon', adminAuth.isLoggedIn, couponController.editCoupon);
+adminRoute.post('/editCouponPost', adminAuth.isLoggedIn, couponController.editCouponPost);
+adminRoute.get('/hideCoupon', adminAuth.isLoggedIn, couponController.hideCoupon);
+adminRoute.get('/showCoupon', adminAuth.isLoggedIn, couponController.showCoupon);
 
 //sales report
 adminRoute.get('/salesReport', adminAuth.isLoggedIn, adminController.salesReport);
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,10 @@
 const userController = require('../controllers/userController');
-const cartController=require('../controllers/cartController');
-const orderController=require('../controllers/orderController');
-const profileController=require('../controllers/profileController');
-const couponController=require('../controllers/couponController');
-const walletController=require('../controllers/walletController');
-const wishlistController=require('../controllers/wishlistController');
+const cartController = require('../controllers/cartController');
+const orderController = require('../controllers/orderController');
+const profileController = require('../controllers/profileController');
+const couponController = require('../controllers/couponController');
+const walletController = require('../controllers/walletController');
+const wishlistController = require('../controllers/wishlistController');
 const userAuth = require('../middleware/userAuth');
 const express = require('express');
 const userRoute = express();
@@ -14,16 +14,16 @@ userRoute.set('views', './views');
 userRoute.set('view engine', 'ejs');
 
 //routes for users
-userRoute.get('/login',userAuth.isLoggedOut, userController.loginGet);
+userRoute.get('/login', userAuth.isLoggedOut, userController.loginGet);
 userRoute.post('/login', userAuth.isLoggedOut, userController.userLoginPost);
-userRoute.get('/forgotPassword',userController.forgotPassword);
-userRoute.post('/forgotPassword',userController.forgotPasswordPost);
-userRoute.get('/forgotPasswordChange',userController.forgotPasswordChange);
-userRoute.post('/forgotPasswordChange',userController.forgotPasswordChangePost);
+userRoute.get('/forgotPassword', userController.forgotPassword);
+userRoute.post('/forgotPassword', userController.forgotPasswordPost);
+userRoute.get('/forgotPasswordChange', userController.forgotPasswordChange);
+userRoute.post('/forgotPasswordChange', userController.forgotPasswordChangePost);
 userRoute.get('/register', userController.registerGet);
 userRoute.post('/registerPost', userController.registerPost);
 userRoute.get('/', userController.landingPage);
-userRoute.get('/products',  userAuth.isLoggedIn, userController.productsGet);
+userRoute.get('/products', userAuth.isLoggedIn, userController.productsGet);
 userRoute.get('/otp', userAuth.isLoggedOut, userAuth.isLoggedIn, userController.otp);
 userRoute.post('/otp', userController.otpPost);
 userRoute.get('/logout', userController.logout);
@@ -32,44 +32,44 @@ userRoute.get('/about', userAuth.isLoggedIn, userController.about);
 userRoute.get('/contact', userAuth.isLoggedIn, userController.contact);
 
 //routes for cart management
-userRoute.get('/cart',  userAuth.isLoggedIn, cartController.cartGet);
+userRoute.get('/cart', userAuth.isLoggedIn, cartController.cartGet);
 userRoute.post('/add', cartController.addToCartPost);
 userRoute.post('/cartModify', cartController.updateCartQuantity);
-userRoute.post('/removeProduct',cartController.removeProduct);
+userRoute.post('/removeProduct', cartController.removeProduct);
 userRoute.get('/checkout', userAuth.isLoggedIn, cartController.checkout);
-userRoute.get('/editAddress',  userAuth.isLoggedIn, cartController.editAddress);
-userRoute.post('/editAddressPost',cartController.editAddressPost);
-userRoute.get('/success',  userAuth.isLoggedIn, cartController.success);
-userRoute.post('/verifyPayment',cartController.verifyPayment);
+userRoute.get('/editAddress', userAuth.isLoggedIn, cartController.editAddress);
+userRoute.post('/editAddressPost', cartController.editAddressPost);
+userRoute.get('/success', userAuth.isLoggedIn, cartController.success);
+userRoute.post('/verifyPayment', cartController.verifyPayment);
 
 //routes for order management
-userRoute.get('/orders',  userAuth.isLoggedIn, orderController.orders);
-userRoute.post('/placeOrder',orderController.placeOrder);
-userRoute.get('/orderDet',  userAuth.isLoggedIn, orderController.orderDet);
-userRoute.get('/cancelOrder',orderController.cancelOrder);
-userRoute.get('/returnOrder',  userAuth.isLoggedIn, orderController.returnOrder);
+userRoute.get('/orders', userAuth.isLoggedIn, orderController.orders);
+userRoute.post('/placeOrder', orderController.placeOrder);
+userRoute.get('/orderDet', userAuth.isLoggedIn, orderController.orderDet);
+userRoute.get('/cancelOrder', orderController.cancelOrder);
+userRoute.get('/returnOrder', userAuth.isLoggedIn, orderController.returnOrder);
 
 //routes for profile management
-userRoute.get('/account',  userAuth.isLoggedIn, profileController.account);
-userRoute.post('/accountPost',profileController.changePasswordPost);
-userRoute.get('/changePassword',  userAuth.isLoggedIn, profileController.changePassword);
-userRoute.get('/address',  userAuth.isLoggedIn, profileController.address);
-userRoute.get('/addAddress',  userAuth.isLoggedIn, profileController.addAddress);
-userRoute.post('/addAddress',profileController.addAddressPost);
-userRoute.get('/adelete/:id',profileController.deleteAddress);
-userRoute.get('/aedit/:id',profileController.editAdd);
-userRoute.post('/editAddPost',profileController.editAddPost);
+userRoute.get('/account', userAuth.isLoggedIn, profileController.account);
+userRoute.post('/accountPost', userAuth.isLoggedIn, profileController.changePasswordPost);
+userRoute.get('/changePassword', userAuth.isLoggedIn, profileController.changePassword);
+userRoute.get('/address', userAuth.isLoggedIn, profileController.address);
+userRoute.get('/addAddress', userAuth.isLoggedIn, profileController.addAddress);
+userRoute.post('/addAddress', userAuth.isLoggedIn, profileController.addAddressPost);
+userRoute.get('/adelete/:id', userAuth.isLoggedIn, profileController.deleteAddress);
+userRoute.get('/aedit/:id', userAuth.isLoggedIn, profileController.editAdd);
+userRoute.post('/editAddPost', userAuth.isLoggedIn, profileController.editAddPost);
 
 //route for coupons
-userRoute.post('/applyCoupon',couponController.applyCoupon);
-userRoute.get('/removeCoupon',couponController.removeCoupon);
+userRoute.post('/applyCoupon', userAuth.isLoggedIn, couponController.applyCoupon);
+userRoute.get('/removeCoupon', userAuth.isLoggedIn, couponController.removeCoupon);
 
 //route for wallet
-userRoute.get('/wallet',  userAuth.isLoggedIn, walletController.wallet);
+userRoute.get('/wallet', userAuth.isLoggedIn, walletController.wallet);
 
 //route for wishlist
-userRoute.get('/wishlist',  userAuth.isLoggedIn, wishlistController.wishlist);
-userRoute.post('/addToWishlist',userAuth.isLoggedIn,wishlistController.addToWishlist);
-userRoute.post('/removeFromWishlist',userAuth.isLoggedIn,wishlistController.removeFromWishlist);
+userRoute.get('/wishlist', userAuth.isLoggedIn, wishlistController.wishlist);
+userRoute.post('/addToWishlist', userAuth.isLoggedIn, wishlistController.addToWishlist);
+userRoute.post('/removeFromWishlist', userAuth.isLoggedIn, wishlistController.removeFromWishlist);
 
 module.exports = userRoute;
